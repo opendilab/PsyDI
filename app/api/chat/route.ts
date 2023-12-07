b00150864f53a3a15f406d7580334fb5c8c468ae
niuyazhe <niuyazhe@sensetime.com>
2023-12-07 15:44:56 +0800

    feature(nyz): add debug env varaible and naive kv

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -34,18 +34,25 @@ export async function POST(req: Request) {
 
   const startTime: Date = new Date();
   const turnCount = userCountDict[userId]
-  let messages_user = messages.filter((message: {[key: string]: string}) => message.role === 'user')
-  const response = await server.create({
-    rawContent: messages_user[turnCount].content,
-    uid: userId,
-    turnCount: turnCount,
-    stream: true,
-    max_tokens: 1000,
-    temperature: 0.75,
-    top_p: 1,
-  })
-  const done = response.done
-  const response_string = response.response_string
+
+  const debug = process.env.DEBUG
+  let response_string = ''
+  if (debug === 'true') {
+    response_string = 'hello world'
+  } else {
+    let messages_user = messages.filter((message: {[key: string]: string}) => message.role === 'user')
+    const response = await server.create({
+      rawContent: messages_user[turnCount].content,
+      uid: userId,
+      turnCount: turnCount,
+      stream: true,
+      max_tokens: 1000,
+      temperature: 0.75,
+      top_p: 1,
+    })
+    const done = response.done
+    response_string = response.response_string
+  }
   const endTime: Date = new Date();
   const elapsedTime: number = endTime.getTime() - startTime.getTime();
   console.log(response_string, `Total elapsed time: ${elapsedTime}ms`)
@@ -58,6 +65,28 @@ export async function POST(req: Request) {
         console.log('Translate API is not available or rate limit exceeded, using original text')
     }
   }
+
+  const title = json.messages[0].content.substring(0, 100)
+  const id = json.id ?? nanoid()
+  const createdAt = Date.now()
+  const path = `/chat/${id}`
+  const payload = {
+        id,
+        title,
+        userId,
+        createdAt,
+        path,
+        turnCount: turnCount,
+        messages: messages,
+        response: response_string,
+  }
+  // console.log(payload)
+  await kv.hmset(`chat:${id}`, payload)
+  await kv.zadd(`user:chat:${userId}`, {
+        score: createdAt,
+        member: `chat:${id}`
+  })
+
   const dataStream = new ReadableStream({
     start(controller) {
         controller.enqueue(encoder.encode(text ? text : response_string_with_newline));
